ae73ba1418355f9d44a07ad516abf6ad344b60f1
Alex Irvine <alex-irvine@hotmail.com>
2016-12-06 10:18:15 +0000

    files deleted

---
+++ continentcreation.sql
@@ -0,0 +1,10 @@
+﻿CReATE TABLE continents (cc nvarchar(2), a_2 nvarchar(2), a_3 nvarchar(3), num int, name nvarchar(max))
+
+BULK
+INSERT continents
+FROM 'c:\Users\s14003221\Desktop\continents.csv'
+WITH
+(
+FIELDTERMINATOR = ',',
+ROWTERMINATOR = '0x0a'
+)
